6ae05ddfa7cff85140bf9e903217ad48143c0d4f
lisenet <tomas@lisenet.com>
2021-12-28 16:13:55 +0000

    Deploy Kubecost using Terraform

---
+++ terraform/k8s-kubecost.tf
@@ -0,0 +1,16 @@
+resource "helm_release" "kubecost" {
+  name       = "kubecost"
+  repository = "https://kubecost.github.io/cost-analyzer/"
+  chart      = "cost-analyzer"
+  namespace  = "kubecost"
+  version    = "1.87.3"
+
+  values = [
+    "${file("../kubecost/values.yaml")}"
+  ]
+
+  depends_on = [
+    kubectl_manifest.calico,
+    kubectl_manifest.prometheus
+  ]
+}
