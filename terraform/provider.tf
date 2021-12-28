748e01d1c2659fc6509581f93941b2c593ee4034
lisenet <tomas@lisenet.com>
2021-12-28 15:25:56 +0000

    Bump Terraform to v1.1 and update providers

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.47.0"
+      version = "3.70.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.2.0"
+      version = "2.4.1"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.3.2"
+      version = "2.7.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = "~> 1.11.2"
+      version = "1.13.1"
     }
   }
 }
