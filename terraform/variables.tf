40cb0b2ced5fe2678e2eca45fefd18b7a67b204e
lisenet <tomas@lisenet.com>
2021-12-28 16:13:27 +0000

    Deploy Elastic Stack using Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "elastic_stack_repository" {
+  description = "Elastic stack Helm repository"
+  type        = string
+  default     = "https://helm.elastic.co"
+}
+
+variable "elastic_stack_version" {
+  description = "Elastic stack Helm chart version"
+  type        = string
+  default     = "7.16.2"
+}
+
+variable "elastic_stack_namespace" {
+  description = "Elastic stack Kubernetes namespace"
+  type        = string
+  default     = "logging"
+}
+
