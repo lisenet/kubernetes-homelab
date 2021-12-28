013c7640081d96bcbb0321fefd0fa9b68087ae6a
lisenet <tomas@lisenet.com>
2021-12-28 16:13:12 +0000

    Use public Helm chart

--- terraform/k8s-x509-certificate-exporter.tf
+++ terraform/k8s-x509-certificate-exporter.tf
@@ -1,7 +1,12 @@
 resource "helm_release" "x509-certificate-exporter" {
   name       = "x509-certificate-exporter"
-  chart      = "../charts/x509-certificate-exporter/"
+  repository = "https://charts.enix.io"
+  chart      = "x509-certificate-exporter"
   namespace  = "monitoring"
+
+  values = [
+    "${file("../x509-certificate-exporter/values.yml")}"
+  ]
 
   depends_on = [
     kubectl_manifest.calico,
