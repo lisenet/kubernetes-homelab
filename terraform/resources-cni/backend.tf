c96083684810ea1cbe5d0467f33f47aff4a7afd6
lisenet <tomas@lisenet.com>
2025-07-08 18:07:51 +0100

    Add terraform/resources-cni

---
+++ terraform/resources-cni/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    region         = "eu-west-2"
+    bucket         = "terraform-homelab-remote-state"
+    key            = "resources-cni.tfstate"
+    dynamodb_table = "terraform-homelab-remote-state-lock"
+    encrypt        = true
+    profile        = "terraform_homelab"
+  }
+}
